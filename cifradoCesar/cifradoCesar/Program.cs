1fc0b4e36442cdb53c02cfc7333706aa89d2d69c
Santiago Zapata <santiago@MacBook-Pro-de-Santiago.local>
2024-05-20 22:36:25 -0300

    updates

---
+++ cifradoCesar/cifradoCesar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+char algoritmoCesar(char letra, int vecesAAplicar, string abecedario)
+{
+    if (letra == ' ')
+    {
+        return ' ';
+    }
+    //Encontrar una letra en un string y devolver su posición
+    int posicion = abecedario.IndexOf(letra);
+    int i = 0;
+    while (i < vecesAAplicar)
+    {
+        if (posicion == abecedario.Length - 1)
+        {
+            posicion = -1;
+        }
+        posicion++;
+        i++;
+    }
+    letra = abecedario[posicion];
+    return letra;
+}
+
+String abecedarioCaesar = "aábcdeéfghiíjklmnñoópqrstuúüvwxyz";
+//El número es 12 en el segundo 0:26
+int vecesAAplicar = 12;
+String frase = "kd wiqd géuúi tédbbúnq kdq wiqd iúígédíqsybyuqu";
+String fraseConAlgoritmo = "";
+
+
+for (int j = 0; j < frase.Length; j++)
+{
+    char remplazo = algoritmoCesar(frase[j], vecesAAplicar, abecedarioCaesar);
+    fraseConAlgoritmo += remplazo;
+}
+
+Console.WriteLine(fraseConAlgoritmo);
